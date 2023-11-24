535a2f490c412692c8b4ed010e89f585a16ece56
Thige254 <thigetj@gmail.com>
2023-11-24 23:34:49 +0300

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,4 @@
-# Puppet Manifest for installing Flask with pip3
-
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python3-pip'],
-}
-
-package { 'python3-pip':
-  ensure => 'latest',
-  provider => 'apt',
 }

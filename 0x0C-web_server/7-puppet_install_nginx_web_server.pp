a5686fd3ad38064b2e458cba5a996e72ea55c819
Thige254 <thigetj@gmail.com>
2023-11-28 19:33:22 +0300

    7777

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,43 @@
+# Install Nginx package
+package { 'nginx':
+  ensure => installed,
+}
+
+# Configure Nginx
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => "
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    server_name _;
+
+    root /var/www/html;
+    index index.html;
+
+    location / {
+        return 200 'Hello World!';
+    }
+
+    location /redirect_me {
+        return 301 'http://tentechnologies.tech$request_uri';
+    }
+}
+",
+  notify  => Service['nginx'],
+}
+
+# Create symbolic link to enable the default site
+file { '/etc/nginx/sites-enabled/default':
+  ensure => link,
+  target => '/etc/nginx/sites-available/default',
+  notify => Service['nginx'],
+}
+
+# Ensure Nginx service is running and enabled
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
+}

485d9fa1b83e37a18436d385c086110d44bb5d0a
ThigeJay <thigetj@gmail.com>
2024-02-08 12:02:13 +0300

    1-ten_.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,6 +4,7 @@ Module to print titles offirst 10 hot posts listed for a given subreddit
 """
 
 from requests import get
+import json
 
 
 def top_ten(subreddit):
@@ -20,15 +21,20 @@ def top_ten(subreddit):
 
     if subreddit is None or not isinstance(subreddit, str):
         print("None")
+        return
 
     user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     params = {'limit': 10}
     url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
 
-    response = get(url, headers=user_agent, params=params)
-    results = response.json()
-
     try:
+        response = get(url, headers=user_agent, params=params)
+        results = response.json()
+
+        if 'error' in results:
+            print("None")
+            return
+
         my_data = results.get('data').get('children')
 
         for i in my_data:
@@ -36,3 +42,9 @@ def top_ten(subreddit):
 
     except Exception:
         print("None")
+
+
+# Test the function with subreddit names
+if __name__ == "__main__":
+    top_ten("programming")
+    top_ten("this_is_a_fake_subreddit")

fa4e58f7909141a93c5cb7079ebb5d5a3146555b
Thige254 <thigetj@gmail.com>
2024-03-05 15:55:24 +0000

    1-top-ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This script retrieves the number of subscribers for a specified Reddit subreddit.
+"""
+
+import requests
+
+
+def get_subreddit_subscribers(subreddit):
+    """Retrieve the total number of subscribers for a specified subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent: linux:my_script:v1.0.0 (by /u/myredditusername)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    data = response.json().get("data")
+    return data.get("subscribers")
